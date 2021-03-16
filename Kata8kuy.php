2d56824f4cbe834559bbe17cb80db0162e9a55ec
idembele70 <idembele70@gmail.com>
2021-03-16 16:59:56 +0100

    removeChar

--- Kata8kuy.php
+++ Kata8kuy.php
@@ -1,7 +1,12 @@
 <?php 
-function makeNegative(float $num)
-{
-    return $num <= 0 ? $num : -$num;
+function removeChar(string $s ="eloquent") : string{
+$sTab =  str_split($s); $news = null;
+     foreach ($sTab as $key => $value) {
+         if ($key == 0 || $key == strlen($s)-1);
+         else $news .= $value;
+     }
+     return $news;
 }
+// order bypopularity
 
-echo makeNegative(-12.98) ;
+echo removeChar();
