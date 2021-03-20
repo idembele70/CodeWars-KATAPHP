b9d3d87f55fe44243e2ef861404f3536d06a771b
idembele70 <idembele70@gmail.com>
2021-03-20 21:28:29 +0100

    avec for if

--- Kata8kuy.php
+++ Kata8kuy.php
@@ -1,6 +1,10 @@
 <?php
 function smallestInteger($arr=[3, 5, 10, 1, 4, 55]) : int {
-    return min($arr);
+    $min = $arr[0];
+    foreach ($arr as $value) {
+      $min > $value ? $min = $value : $min;
+    }
+    return $min;
 }
 
 echo smallestInteger();
