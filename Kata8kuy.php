40c7bd350ab016dcc98c200664c1c742af355696
idembele70 <idembele70@gmail.com>
2021-04-07 23:25:04 +0200

    litre une ligne

--- Kata8kuy.php
+++ Kata8kuy.php
@@ -1,25 +1,5 @@
 <?php
-function basicOp($op='+', $val1=4, $val2=5): int
-{
-{{    switch ($op) {
-        case '+':
-            $res = $val1 + $val2;
-            break;
-        case '-':
-            $res = $val1 - $val2;
-            break;
-        case '*':
-            $res = $val1 * $val2;
-            break;
-        case '/':
-            $res = $val1 / $val2;
-            break;
-
-        default:
-            "This operator doesn't exist !";
-            break;
-    }
-    return $res;
+function litres($t){
+    return floor($t * 0.5);
 }
-}}
-echo basicOp();
+echo litres(5);
