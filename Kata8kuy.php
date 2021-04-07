31e9dbeae9874ca85909621992429836542184ac
idembele70 <idembele70@gmail.com>
2021-04-07 22:30:18 +0200

    centuryfromYear

--- Kata8kuy.php
+++ Kata8kuy.php
@@ -1,27 +1,6 @@
 <?php
-function squareSum($numbers): int
-{
-    $sum = null;
-    if ($numbers != null) {
-        for ($i=0; $i < sizeof($numbers); $i++) { 
-          $sum +=  $numbers[$i] * $numbers[$i];
-        }
-    } else {
-        $sum = 0;
-    }
-    return $sum;
+function centuryFromYear($year=1705): int{
+    return round(ceil($year +99)/100);
 }
 
-
-
-echo squareSum([1, 2]);
-echo squareSum([]);
-
-/* 
-function sum($carry, $index)
-    {
-        $carry += $index * $index;
-        return $carry;
-    }
-   return $numbers != null ? array_reduce($numbers, "sum") : 0;
-    */
+echo centuryFromYear();
